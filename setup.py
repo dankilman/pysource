8cb96e291acd40fa4bc0961cb9d2192740fe4e04
Dan Kilman <dankilman@gmail.com>
2014-05-08 19:52:07 +0300

    initial setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pysource',
+    version='0.1',
+    author='Dan Kilman',
+    author_email='dankilman@gmail.com',
+    packages=['pysource'],
+    description='Execute python functions from bash',
+    zip_safe=False,
+    install_requires=[],
+    entry_points = {}
+)
