3067f4c84458a9df3026c057c0963e7c1bcce928
Dan Kilman <dankilman@gmail.com>
2014-05-09 20:15:46 +0300

    add minimal sample code

---
+++ pysource/test/res/example.py
@@ -0,0 +1,14 @@
+from pysource import function
+
+@function
+def fun1(name, name2, name3):
+    return 'name: {0}, name2: {1}, name3: {2}'.format(name, name2, name3)
+
+@function
+def fun2(name):
+    return 'name: {0}'.format(name)
+
+@function
+def fun3(name):
+    return 'fun3: {0}'.format(name)
+
