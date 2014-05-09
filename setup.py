3859c44f291226370375f83e419e75f3705a18a5
Dan Kilman <dankilman@gmail.com>
2014-05-10 02:28:49 +0300

    add license headers

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+# Copyright 2014 Dan Kilman
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from setuptools import setup
 
 setup(
@@ -5,6 +19,7 @@ setup(
     version='0.1',
     author='Dan Kilman',
     author_email='dankilman@gmail.com',
+    license='LICENSE',
     packages=['pysource'],
     scripts=['pysource.sh'],
     description='Execute python functions from bash',
