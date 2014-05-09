881ba5d99a6266369dbd2c14068ecf828325db8d
Dan Kilman <dankilman@gmail.com>
2014-05-10 01:42:09 +0300

    put script in bin during installation

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     author='Dan Kilman',
     author_email='dankilman@gmail.com',
     packages=['pysource'],
+    scripts=['pysource.sh'],
     description='Execute python functions from bash',
     zip_safe=False,
     install_requires=[
         'argh',
         'python-daemon'
     ],
-    entry_points={}
 )
