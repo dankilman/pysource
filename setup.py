b3ab7f6a17ee58fa55fd0351c1915c935d2645f3
Dan Kilman <dankilman@gmail.com>
2014-05-18 21:56:09 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -32,13 +32,13 @@ setup(
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators'
+        'Intended Audience :: System Administrators',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2 :: Only',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Unix Shell',
-        'Topic :: Software Development ::'
+        'Topic :: Software Development',
         'Topic :: System :: Shells',
         'Topic :: System :: System Shells',
         'Topic :: Utilities',
